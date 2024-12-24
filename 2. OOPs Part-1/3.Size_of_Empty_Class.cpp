ea5c8218cc16209c75c584aa22de4dd046e87743
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-24 13:39:01 +0530

    Understand Size of Empty Class

---
+++ 2. OOPs Part-1/3.Size_of_Empty_Class.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//* Size of Empty class
+
+class Student
+{
+};
+
+int main()
+{
+    cout << sizeof(Student) << endl;
+    // Output: 1
+
+    return 0;
+}
