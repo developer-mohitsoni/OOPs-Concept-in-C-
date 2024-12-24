d7a7d43f964971f954f46400c064f26bbc5c8ff5
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-24 13:38:31 +0530

    Without Class Demonstration

---
+++ 2. OOPs Part-1/1.Without_Class.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//* Without OOP's
+
+int main()
+{
+    int id1, id2;
+    string name1, name2;
+    int age1, age2;
+    int nos1, nos2;
+    int marks1[5], marks2[5];
+
+    return 0;
+}
