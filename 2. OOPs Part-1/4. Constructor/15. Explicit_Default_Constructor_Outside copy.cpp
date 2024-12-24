10ebcac87179ec33c8ede1e6dd13481a5e497ced
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-24 15:23:48 +0530

    Program of Defining Explicit Default Constructor Outside the Class

---
+++ 2. OOPs Part-1/4. Constructor/15. Explicit_Default_Constructor_Outside copy.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//* Defining Explicit Default Constructor Outside the Class
+
+class Student
+{
+private:
+    string name;
+    int age;
+
+public:
+    // Explicit Default constructor
+    Student();
+
+    // Getter Methods Define Outside the Class:-
+    string getName() const;
+    int getAge() const;
+};
+
+//! NOTE:- Here you can easily access the private data members and members functions of this class directly by using the Explicit Default Constructor and Parameterized Constructor but not using Default Constructor
+//* Defining Explicit Default Constructor
+Student::Student()
+{
+    // Explicit Default Constructor
+    cout << "Enter the Name" << endl; // Asking for input
+    cin >> name;
+
+    cout << "Enter the Age" << endl; // Asking for input
+    cin >> age;
+}
+string Student::getName() const { return name; }
+
+int Student::getAge() const { return age; }
+
+int main()
+{
+    Student s1;
+    cout << "Name: " << s1.getName() << endl;
+    cout << "Age: " << s1.getAge() << endl; // Since we have getter, we can directly access the private member.
+
+    return 0;
+}
