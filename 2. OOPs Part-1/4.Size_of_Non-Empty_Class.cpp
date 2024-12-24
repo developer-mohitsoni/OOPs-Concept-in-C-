c77040154a6a1f5e89b5321023e60b78a2efee59
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-24 13:39:18 +0530

    Understand size of Non-Empty Class

---
+++ 2. OOPs Part-1/4.Size_of_Non-Empty_Class.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//* Size of Non-Empty class
+
+class MyClass
+{
+    int a;
+    float b;
+};
+
+int main()
+{
+    cout << sizeof(MyClass) << endl;
+    // Output depends on System Architecture
+
+    return 0;
+}
