b157daaa5e0ec19e26e65c0a15844ff1dd0cc2e3
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-27 15:47:07 +0530

    Program of Inheritance:- Public Member inherits as Protected Mode

---
+++ 3. OOPs Part-2/9. Abstraction/9.2. Inheritance/9.2.1. Mode of Inheritance/35. Base-Public_Derived-protected.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Public Member inherits as Public Mode
+
+class Animal
+{
+public:
+    int age;
+
+    void eat()
+    {
+        cout << "Animal is eating" << endl;
+    }
+};
+
+// Derived Class
+
+//! All public and protected members of the base class (Animal) become protected members in the derived class (Dog).
+
+//! Private members of the base class are never accessible directly in the derived class, regardless of the access specifier.
+class Dog : protected Animal
+{
+public:
+    void bark()
+    {
+        cout << "Dog is barking" << endl;
+    }
+};
+
+int main()
+{
+    Dog dogObj;
+
+    //* No error occured by while accessing protected data member and member function withing the Derived Class.
+    dogObj.bark();
+
+    //! Error is occured as we are accessing public data member and member function in the Derived Class as protected.
+
+    // dogObj.age = 8;
+    // dogObj.eat();
+
+    return 0;
+}
