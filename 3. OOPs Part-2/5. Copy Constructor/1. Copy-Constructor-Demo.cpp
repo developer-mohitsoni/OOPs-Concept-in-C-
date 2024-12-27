96dcf5933d81900c102d6e66b0b0328811ce8ddb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-27 12:59:10 +0530

    Understand Copy Constructor

---
+++ 3. OOPs Part-2/5. Copy Constructor/1. Copy-Constructor-Demo.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//* NOTE:- Copy Constructor ka koi return type nai hota hai.
+
+class Student
+{
+private:
+    string gf;
+
+    void chatting()
+    {
+        cout << "Chatting" << endl;
+    }
+
+public:
+    int id;
+    string name;
+    int age;
+    string gender;
+
+    // Default Constructor: assign garbage value
+
+    Student()
+    {
+        cout << "Default Constructor Called" << endl;
+    }
+
+    // Parameterized Constructor: assign sensible value
+
+    Student(int _id, string _name, int _age, string _gender, string _gf)
+    {
+        id = _id;
+        name = _name;
+        age = _age;
+        gender = _gender;
+        gf = _gf;
+
+        cout << "Parameterized Constructor Called for " << name << endl;
+    }
+
+    void study()
+    {
+        cout << "Studying" << endl;
+    }
+};
+
+int main()
+{
+    Student s1(104, "Love", 25, "Male", "Lovely");
+
+    Student s2; // Compiler added default constructor
+
+    s2 = s1; // s1 compied into s2
+
+    cout << s2.age << " " << s2.name << endl;
+
+    return 0;
+}
