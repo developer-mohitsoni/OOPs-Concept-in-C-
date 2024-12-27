1bf7ae85212ea2666fb048da3b8d05acf2520555
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-27 15:46:46 +0530

    Program of Inheritance:- Public Member inherits as Private Mode

---
+++ 3. OOPs Part-2/9. Abstraction/9.2. Inheritance/9.2.1. Mode of Inheritance/34. Base-Public_Derived-private.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Public Member inherits as Public Mode
+
+class Animal
+{
+public:
+    int age;
+
+    void eat()
+    {
+        cout << "Animal is eating" << endl;
+    }
+};
+
+// Derived Class
+
+//! All public and protected members of the base class become private members of the derived class.
+
+//! Private members of the base class are not inherited by the derived class directly.
+
+//! The inherited members can only be accessed within the derived class or its member functions, not by objects of the derived class.
+
+class Dog : private Animal
+{
+public:
+    void bark()
+    {
+        cout << "Dog is barking" << endl;
+    }
+};
+
+int main()
+{
+    Dog dogObj;
+
+    //* No error occured by while accessing private data member and member function withing the Derived Class.
+    dogObj.bark();
+
+    //! Error is occured as we are accessing public data member and member function in the Derived Class as private.
+
+    // dogObj.age = 8;
+    // dogObj.eat();
+
+    return 0;
+}
