69e2dc9d9f47d467c8e723450547b9191787f5cb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-28 14:52:08 +0530

    Program Example to show How Shallow Copy lead to Dangling Pointer fix by using Deep Copy

---
+++ 3. OOPs Part-2/10. Shallow Copy vs Deep Copy/Deep Copy/59. Example-how-Shallow-Copy-Lead-Dangling-Pointer-fix-by-Deep-Copy.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//* Program Example to show How Shallow Copy lead to Dangling Pointer fix by using Deep Copy
+
+class MyClass
+{
+public:
+    int *myPointer;
+
+    MyClass() : myPointer(new int(20)) {};
+
+    MyClass(const MyClass &other)
+    {
+        myPointer = new int(*other.myPointer); // Deep copy
+    }
+
+    ~MyClass()
+    {
+        delete myPointer;
+    }
+};
+
+int main()
+{
+    MyClass original;            // Create original object
+    MyClass DeepCopy = original; // Create a deep copy
+
+    // Modify the value in the original
+    *original.myPointer = 100;
+
+    // Check that DeepCopy is independent
+    cout << "Original Value: " << *original.myPointer << endl;  // Should print 100
+    cout << "Deep Copy Value: " << *DeepCopy.myPointer << endl; // Should print 42
+
+    return 0;
+}
