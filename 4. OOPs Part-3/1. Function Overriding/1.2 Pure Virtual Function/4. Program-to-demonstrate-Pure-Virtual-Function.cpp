7f30e73f6d7ab315252ef692a60f785468a1da7d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-28 16:26:12 +0530

    Program to demonstrate Pure Virtual Function.

---
+++ 4. OOPs Part-3/1. Function Overriding/1.2 Pure Virtual Function/4. Program-to-demonstrate-Pure-Virtual-Function.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//! Program to demonstrate Pure Virtual Function.
+
+class Bird // Interface
+{
+public:
+    // Pure Virtual Function
+
+    virtual void eat() = 0;
+    virtual void fly() = 0;
+
+    // classes that inherit this class has to implement pure virtual function
+
+    // Pure Virtual Destructor (Recommended)
+    virtual ~Bird() {}
+};
+
+class Sparrow : public Bird // Implement
+{
+public:
+    void eat()
+    {
+        cout << "Sparrow is eating" << endl;
+    }
+    void fly()
+    {
+        cout << "Sparrow is flying" << endl;
+    }
+
+    ~Sparrow() {}
+};
+
+void birdDoesSomething(Bird *&bird)
+{
+    bird->eat();
+    bird->fly();
+}
+
+int main()
+{
+    Bird *bird = new Sparrow(); // Ye dena jaroori hai ki kon si bird hum use kar rahe hai.
+
+    birdDoesSomething(bird); // Bird pointer ko hum use kar rahe hai.
+
+    delete bird; // Memory leak aayega.
+
+    return 0;
+}
