8f22dff882370fb1c42d15232c686ba458af975e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-29 14:05:55 +0530

    Introduction to know about Constructor

---
+++ 5. Revision Series of All Concepts/15. Constructor/1. Intoduction-to-Constructor.cpp
@@ -0,0 +1,24 @@
+//! Constructor:-
+// Constructor are Special member function which is used to initialize value of a variable inside an object.
+
+//! The major points about Constructor:-
+// 1. Constructor has same name as the class.
+// 2. Constructor has no return type not even void.
+// 3. Constructor is automatically called when an object is created.
+// 4. Constructor is used to initialize the values of the data members of a class.
+// 5. Constructor can be overloaded.
+// 6. Constructor can be declared as static.
+// 7. Constructor can be declared as virtual.
+// 8. Constructor can be declared as friend.
+// 9. Constructor allow Default Argument Concept.
+// 10. Constructor can't be inherited.
+// 11. Constructor are defined inside public section.
+
+//! Types of Constructor:-
+/*
+1. Default Constructor
+2. Parameterized Constructor
+3. Copy Constructor
+4. Move Constructor
+5. Initializer List Constructor
+*/
