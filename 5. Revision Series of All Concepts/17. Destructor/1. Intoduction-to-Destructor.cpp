807245dadfe7ee314a828beb1f7a0b4480969cf1
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-29 15:12:17 +0530

    Introduction to Destructor

---
+++ 5. Revision Series of All Concepts/17. Destructor/1. Intoduction-to-Destructor.cpp
@@ -0,0 +1,14 @@
+//! Destructor:-
+// Destructor is a special member function of a class which is automatically called when an object of that class is destroyed
+// Destructor is used to release the resources which are acquired by the class
+
+//! The major points about Destructor:-
+// 1. Destructor is a special member function of a class which is automatically called when an object of that class is destroyed.
+// 2. Destructor is used to release the resources which are acquired by the class.
+// 3. Destructor is a member function of a class which has a prefix ~ (Tilde is used to refer to the destructor function of the class itself)
+// 4. Destructor does not take any parameters and does not return any value.
+// 5. Destructor can be overloaded.
+// 6. Destructor can be declared as static.
+// 7. Destructor can be declared as virtual.
+// 8. Destructor can be declared as pure virtual.
+// 9. Destructor can be declared as friend.
