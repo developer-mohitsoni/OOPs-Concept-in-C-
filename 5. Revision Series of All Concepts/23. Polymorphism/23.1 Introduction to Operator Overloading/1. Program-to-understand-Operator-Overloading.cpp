49a9fc76fae199acdf746490e835d4cc4838107a
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 19:45:54 +0530

    Introduction to Operator Overloading

---
+++ 5. Revision Series of All Concepts/23. Polymorphism/23.1 Introduction to Operator Overloading/1. Program-to-understand-Operator-Overloading.cpp
@@ -0,0 +1,16 @@
+//! Operator Overloading
+
+// Operator Overloading is one of the most important feature of C++ which is a type of Polymorphism.
+
+// Using the concept of Operator Overloading, we can overload any built-in operator i.e we can assign a new definition of existing operators.
+
+//! Every Operator can be Overload except these below:-
+
+// 1. Assignment operator (=)
+// 2. Member access operator (.)
+// 3. Scope resolution operator (::)
+// 4. sizeof operator (sizeof)
+// 5. typeid operator (typeid)
+// 6. Comprehension operator (?:)
+// 7. Comma operator (,)
+// 8. Astric operator (*)
