0ffda7bb9c7c9d87e146cd65f5bc5662cbe3fdfc
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 13:30:48 +0530

    Introduction to Inheritance

---
+++ 5. Revision Series of All Concepts/18. Inheritance/1. Introduction-to-Inheritance.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//! Inheritance:-
+// Inheritance is a way one of the most important features of OOP. This provides us the capability to use the pre-existing code to our new project/software.
+
+// Because it is always better to reuse the same code rather than developing all over again. This re-usability feature is incorporated by the concept of Inheritance.
+
+//! Types of Inheritance:-
+
+// 1. Single Inheritance:- This is the most common type of inheritance where a class inherits from only one class.
+
+// 2. Multilevel Inheritance:- This is a type of inheritance where a class inherits from another class. This class is then inherited from another class.
+
+// 3. Multiple Inheritance:- This is a type of inheritance where a class can inherit from multiple classes.
+
+// 4. Hierarchical Inheritance:- This is a type of inheritance where a class can inherit from multiple classes and each class can inherit from another class.
+
+// 5. Hybrid Inheritance:- This is a type of inheritance where a class can inherit from multiple classes and also inherit from another class.
+
+//! Advantages of Inheritance:-
+// 1. Code Reusability:- Inheritance allows us to reuse the code of the parent
+
+// 2. Encapsulation and Modularity:- Inheritance helps to encapsulate the data and behavior of the class into a single unit.
+
+// 3. Abstraction:- Inheritance helps to achieve abstraction by hiding the implementation details of the
+
+// 4. Polymorphism:- Inheritance allows us to achieve polymorphism by providing different implementations of the same method.
+
+// 5. Easier Maintenance:- Inheritance makes it easier to maintain the code as any changes.
+
+// 6. Improved Readability:- Inheritance improves the readability of the code as it clearly shows
+
+// 7. Reduced Code Size:- Inheritance reduces the code size as it allows us to reuse
+
+// 8. Improved Flexibility:- Inheritance improves the flexibility of the code as it allows us
+
+// 9. Improved Extensibility:- Inheritance improves the extensibility of the code as
