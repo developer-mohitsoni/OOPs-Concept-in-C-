c995af1e616967d73afda5ff16cc9a00b08cf0f6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 13:52:35 +0530

    Understand Private Protected and Public Access Specifier in Inheritance

---
+++ 5. Revision Series of All Concepts/18. Inheritance/18.1 Private-Protected-Public-Inheritance/Understand-Private-Protected-and-Public-Inheritance.cpp
@@ -0,0 +1,6 @@
+//! BASE CLASS
+                       // Private            Protected                Public
+//! Derived Class
+// Private             No access             Private                  Public
+// Protected           No access             Protected                Protected
+// Public              No access            Protected                Public
