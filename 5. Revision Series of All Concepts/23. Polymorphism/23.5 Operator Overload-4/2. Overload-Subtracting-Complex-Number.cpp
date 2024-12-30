7cfb83e8eef286337f1af90039c0a830a2518dbb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 20:41:33 +0530

    Program to understand Operator Overload of Subtracting Complex Number

---
+++ 5. Revision Series of All Concepts/23. Polymorphism/23.5 Operator Overload-4/2. Overload-Subtracting-Complex-Number.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//! Operator Overloading:- Subtracting Complex Number
+
+class Complex
+{
+private:
+    int r, i;
+
+public:
+    void getData()
+    {
+        cout << "Enter the real part of a: ";
+        cin >> r;
+
+        cout << "Enter the imaginary part of a: ";
+        cin >> i;
+    }
+
+    void putData()
+    {
+        cout << "Real = " << r << "\tImag = " << i << endl;
+    }
+
+    Complex operator-(Complex obj)
+    {
+        Complex temp;
+
+        temp.r = r - obj.r;
+        temp.i = i - obj.i;
+
+        return temp;
+    }
+};
+
+int main()
+{
+    Complex a, b, c;
+
+    a.getData();
+    a.putData();
+
+    b.getData();
+    b.putData();
+
+    c = a - b;
+
+    cout << "Sum of Complex Numbers:" << endl;
+    c.putData();
+
+    return 0;
+}
