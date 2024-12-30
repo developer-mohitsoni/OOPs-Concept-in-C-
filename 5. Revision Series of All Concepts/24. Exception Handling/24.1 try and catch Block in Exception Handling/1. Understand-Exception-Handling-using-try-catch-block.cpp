3ce62b7fff6ee2ee300172e9b4e62c252843e85b
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 21:03:23 +0530

    Understand Exception Handling using try and catch() block

---
+++ 5. Revision Series of All Concepts/24. Exception Handling/24.1 try and catch Block in Exception Handling/1. Understand-Exception-Handling-using-try-catch-block.cpp
@@ -0,0 +1,9 @@
+//! Exception Handling:- try and catch Block
+
+//* try:
+// This block contains the code in which we put the code that might contains exceptions.
+
+//* catch:
+// Whenever some exception is found in try block, it is immediately thrown to catch() block.
+
+// Now catch() block will have the responsibility to handle the exception.
