5784978d8d18845453e4da6540b0ac24c0522a58
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 20:57:19 +0530

    Introduction to Exception Handling

---
+++ 5. Revision Series of All Concepts/24. Exception Handling/1. Introduction-to-Exception-Handling.cpp
@@ -0,0 +1,15 @@
+//! Exception Handling
+
+// Exceptions are some unpredictable circumstances when our program terminates suddenly with some errors/issues.
+
+// Exception Handling provides us the facility to handle the exception. So that our program keeps running.
+
+// These happens during running of the program.
+
+//? Example: Trying to divide by zero.
+
+// Exception handling is used to handle these errors gracefully.
+
+//! C++ provides two types of exception handling:
+//* 1. Try-Catch Block
+//* 2. Throw-Catch Block
