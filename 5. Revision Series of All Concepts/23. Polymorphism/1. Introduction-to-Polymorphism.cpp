adab5b4d01faac8bf6f16ec7da3d829f45e4daa0
Mohit Soni <developer_mohitsoni@outlook.com>
2024-12-30 19:39:44 +0530

    Introduction to Polymorphism

---
+++ 5. Revision Series of All Concepts/23. Polymorphism/1. Introduction-to-Polymorphism.cpp
@@ -0,0 +1,23 @@
+//! Introduction to Polymorphism
+
+// It is one of the most important feature of OOP which simply implies that one name having multiple forms.
+
+//! Type of polymorphism:-
+
+//* 1. Compile Time Polymorphism (Static Binding):- In static binding, function calls are resolved at compile time.
+
+//  Function Overloading or Method Overloading, Constructor Overloading
+
+//  Operator Overloading, Virtual Functions
+
+//* 2. Run Time Polymorphism (Dynamic Binding):- In dynamic binding, function calls are resolved at runtime.
+
+//  Virtual Functions
+
+//! Polymorphism is achieved through two ways:
+
+// 1. Overloading: This is a feature where a function name is used with different parameters to achieve different functionalities.
+
+// 2. Overriding: This is a feature where a derived class provides a specific implementation of a method that is already defined in the base class.
+
+//! Note: Polymorphism is not possible with function templates.
